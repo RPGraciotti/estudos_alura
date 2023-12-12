d73c8aac88654628d60fd93f4f90ca5ed2388bba
Rodolfo P Graciotti <rodolfo.graciotti@gmail.com>
2023-12-12 10:50:52 -0300

    aula 5

--- SQL/Conhecendo_SQL/Modulo_1/aula_4.sql
+++ SQL/Conhecendo_SQL/Modulo_1/aula_4.sql
@@ -1,5 +1,7 @@
 -- Consultando dados
 
+-- Filtros
+
 SELECT * FROM tabela_pedidos WHERE TotaldoPedido >= 200;
 
 SELECT * FROM tabela_pedidos WHERE TotaldoPedido <> 200;
@@ -10,5 +12,40 @@ SELECT * FROM tabela_clientes WHERE Nome_Cliente > 'C';
 
 SELECT * FROM tabela_pedidos WHERE DatadoPedido > '2023-09-19';
 
+-- Filtros compostos
 
+SELECT * FROM tabela_pedidos WHERE TotaldoPedido >= 200 AND Status = 'Pendente';
 
+SELECT * FROM tabela_pedidos WHERE Status = 'Pendente' OR Status = 'Processando';
+
+SELECT * FROM tabela_pedidos WHERE NOT Status = 'Pendente'
+
+-- ou
+
+SELECT * FROM tabela_pedidos WHERE Status != 'Pendente'
+
+-- ou
+
+SELECT * FROM tabela_pedidos WHERE Status <> 'Pendente'
+
+-- funcionam igualmente
+
+SELECT * FROM tabela_pedidos WHERE DatadeEnvioEstimada BETWEEN '2023-08-01' AND '2023-09-01'
+
+-- Ordenando com order by
+
+SELECT * FROM tabela_produtos WHERE Preco_de_Compra BETWEEN 200 AND 600 ORDER BY Nome_do_Produto
+
+SELECT * FROM tabela_produtos WHERE Preco_de_Compra BETWEEN 200 AND 600 ORDER BY Preco_de_Compra
+
+-- ASC é o padrão
+
+SELECT * FROM tabela_produtos WHERE Preco_de_Compra BETWEEN 200 AND 600 ORDER BY Data_de_Inclusão DESC
+
+SELECT * FROM tabela_produtos WHERE Preco_de_Compra BETWEEN 200 AND 600 ORDER BY Data_de_Inclusão ASC
+
+SELECT * FROM tabela_produtos WHERE Preco_de_Compra BETWEEN 200 AND 600 ORDER BY Fornecedor DESC
+
+-- Alias: "apelidos"
+-- Apenas na consulta
+SELECT Informacoes_de_Contato AS email_cliente FROM tabela_clientes
---
+++ SQL/Conhecendo_SQL/Modulo_1/aula_5.sql
@@ -0,0 +1,49 @@
+-- Atualizando e excluindo dados
+
+-- Alterar processados para enviados
+
+UPDATE tabela_pedidos SET Status = 'Enviado' WHERE Status = 'Processando'
+
+-- atualize a tabela tornando coluna igual a y onde a coluna for x
+-- sobrescreve e modifica a tabela
+
+SELECT * FROM tabela_pedidos
+
+-- Alterando apenas uma linha
+-- Cliente 2 mudou de endereço e email
+
+UPDATE tabela_clientes SET Informacoes_de_Contato = 'j.santos@email.com', 
+Endereco_cliente = 'Rua dos paralelepípedos, 30'
+WHERE ID_Cliente = 2;
+
+SELECT * FROM tabela_clientes
+
+-- Deletando com DELETE
+
+SELECT * FROM tabela_fornecedores WHERE PaísdeOrigem = 'Turquia'
+
+DELETE FROM tabela_fornecedores WHERE PaísdeOrigem = 'Turquia'
+
+SELECT PaísdeOrigem from tabela_fornecedores ORDER BY PaísdeOrigem
+
+-- Também sobrescreve o conjunto de dados
+
+SELECT ID FROM tabela_fornecedores
+
+DELETE FROM tabela_fornecedores WHERE ID > 35
+
+SELECT ID FROM tabela_fornecedores
+
+-- Delete Cascade https://cursos.alura.com.br/course/sqlite-online-conhecendo-instrucoes-sql/task/145354
+
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(50)
+);
+
+CREATE TABLE Pedidos (
+    PedidoID INT PRIMARY KEY,
+    ClienteID INT,
+    Descricao VARCHAR(100),
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID) ON DELETE CASCADE
+);
